1ee68de7d2a6f033c23ed7511f3b1874c218d25e
Luis <84397555+LuisFadini@users.noreply.github.com>
2024-09-20 19:55:01 -0300

    Improved font and css loading

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,13 @@
+import cssnano from "cssnano";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+  plugins: [
+    tailwindcss(),
+    autoprefixer(),
+    cssnano({
+      preset: "default",
+    }),
+  ],
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 /* @refresh reload */
 import { render } from 'solid-js/web'
-import "@fontsource/inter"
 
 import './index.css'
 import App from './App'
